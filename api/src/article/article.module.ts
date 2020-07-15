0836f5fadc0ed5846bb70c975b01f17cac5663f5
vetordev <vitor13m2003@gmail.com>
2020-07-14 21:28:42 -0300

    entidades do módulo >Article<

--- api/src/article/article.module.ts
+++ api/src/article/article.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from './entity/article.entity';
+import { LikeArticle } from './entity/like-article.entity';
+import { ThemeArticle } from './entity/theme-article.entity';
+import { ArticleImg } from './entity/article-img.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ Article, LikeArticle, ThemeArticle, ArticleImg ])],
   controllers: [ArticleController],
   providers: [ArticleService]
 })
