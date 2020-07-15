777423e51070d0dd394541efe5f94e5e7b31c9ce
vetordev <vitor13m2003@gmail.com>
2020-07-14 22:00:44 -0300

    controller do módulo >Article<

--- api/src/article/article.controller.ts
+++ api/src/article/article.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Res, Req, Post, ParseArrayPipe, Delete, HttpCode } from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { GetArticleDto, GetArticlesByThemeDto, CreateArticleLikeDto, DeleteArticleLikeDto } from './article.dto';
 
 @Controller('article')
-export class ArticleController {}
+export class ArticleController {
+
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Get('article_id')
+  getArticle(@Res() response, @Param() params: GetArticleDto) {
+    return this.articleService.getArticle(response, params.article_id);
+  }
+
+  @Get('like')
+  @HttpCode(200)
+  getArticleByLike() {
+    return this.articleService.getArticlesByLike();
+  }
+
+  @Get('user/like')
+  getArticlesByUserLike(@Req() request) {
+    return this.articleService.getArticlesByUserLike(request.user.user_id);
+  }
+
+  @Get('theme/:theme_id:/like')
+  getArticlesByTheme(@Res() response, @Param() params: GetArticlesByThemeDto) {
+    return this.articleService.getArticlesByTheme(response, params.theme_id);
+  }
+
+  @Post('article/:article_id/like')
+  createArticleLike(@Res() response, @Param() params: CreateArticleLikeDto) {
+    return this.articleService.createArticleLike(response, params.article_id);
+  }
+
+  @Delete('article/:article_id/like')
+  deleteArticleLike(@Res() response, @Param() params: DeleteArticleLikeDto) {
+    return this.articleService.deleteArticleLike(response, params.article_id);
+  }
+
+}
--- api/src/article/article.service.ts
+++ api/src/article/article.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from './entity/article.entity';
+import { Repository } from 'typeorm';
+import { ThemeArticle } from './entity/theme-article.entity';
+import { LikeArticle } from './entity/like-article.entity';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+
+  constructor(
+    @InjectRepository(Article) private readonly articleRespository: Repository<Article>,
+    @InjectRepository(ThemeArticle) private readonly themeArticleRespository: Repository<ThemeArticle>,
+    @InjectRepository(LikeArticle) private readonly likeArticleRespository: Repository<LikeArticle>
+  ) {}
+
+  async getArticle(response: Response, article_id: number): Promise<Article> {
+
+  }
+
+  async getArticlesByLike(): Promise<Article[]> {
+
+  }
+
+  async getArticlesByUserLike(user_id: number): Promise<Article[]> {
+
+  }
+
+  async getArticlesByTheme(response: Response, theme_id: number): Promise<Article[]> {
+
+  }
+
+  async createArticleLike(response: Response, article_id: number): Promise<void> {
+
+  }
+  async deleteArticleLike(response: Response, article_id: number): Promise<void> {
+
+  }
+}
