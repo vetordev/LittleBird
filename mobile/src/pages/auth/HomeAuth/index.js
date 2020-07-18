4813b1292c717c4f0fcfe2367e2e7e3a15bbd132
unknown <heyvitoria.lopes@gmail.com>
2020-07-18 09:12:15 -0300

    páginas de autenticação e home criadas

---
+++ mobile/src/pages/auth/HomeAuth/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const HomeAuth = () => {
+  return <View />;
+}
+
+export default HomeAuth;
---
+++ mobile/src/pages/auth/SignUp2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignUp2 = () => {
+  return <View />;
+}
+
+export default SignUp2;
