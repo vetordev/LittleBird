2017e164c8e39b330bfeb10b5e470a8f191368d5
unknown <heyvitoria.lopes@gmail.com>
2020-07-18 09:24:06 -0300

    rotas de autenticação criadas

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes/auth.routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Routes />
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/routes/auth.routes.js
@@ -0,0 +1,21 @@
+// rotas que o usuário tem acesso enquanto ainda não está autenticado na aplicação (telas de login/cadastro)
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeAuth from '../pages/auth/HomeAuth';
+import SignIn from '../pages/auth/SignIn';
+import SignUp1 from '../pages/auth/SignUp1';
+import SignUp2 from '../pages/auth/SignUp2';
+
+const AuthStack = createStackNavigator();
+
+const Routes = () => (
+   <AuthStack.Navigator>
+      <AuthStack.Screen name="HomeAuth" component={HomeAuth} />
+      <AuthStack.Screen name="SignIn" component={SignIn} />
+      <AuthStack.Screen name="SignUp1" component={SignUp1} />
+      <AuthStack.Screen name="SignUp2" component={SignUp2} />
+   </AuthStack.Navigator>
+);
+
+export default Routes;
