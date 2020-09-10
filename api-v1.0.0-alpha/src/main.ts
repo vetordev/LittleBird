bfa45b6825b4eb09fc1b1bb95057a0c95f024b23
vetordev <vitor13m2003@gmail.com>
2020-09-10 17:05:46 -0300

    refactor: adaptações para deploy no heroku

--- api-v1.0.0-alpha/src/main.ts
+++ api-v1.0.0-alpha/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
 
