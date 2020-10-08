57d0fd989dbf8050676b7726f07447d9eff06767
vetordev <vitor13m2003@gmail.com>
2020-10-08 15:44:06 -0300

    feat(article): Entidade recommendation_article
    API@1.0.0-alpha.10

---
+++ api/src/article/entity/recommendation-article.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Article } from "./article.entity";
+
+@Entity('recommendation_article')
+export class RecommendationArticle {
+
+  @PrimaryGeneratedColumn()
+  recommendation_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 200
+  })
+  recommendation_url: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50
+  })
+  recommendation_type: string;
+
+  @ManyToOne(type => Article, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({
+    name: 'article_id',
+    referencedColumnName: 'article_id'
+  })
+  @Column({
+    type: 'integer'
+  })
+  article_id: number;
+}
