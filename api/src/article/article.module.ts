1ac01964344462537806130d854b025c207ed2f9
vetordev <vitor13m2003@gmail.com>
2020-10-25 13:14:59 -0300

    fix(article): Ajuste na rota de envio de artigos por tema

--- api/src/article/article.module.ts
+++ api/src/article/article.module.ts
@@ -9,9 +9,10 @@ import { ArticleImg } from './entity/article-img.entity';
 import { LaterArticle } from "./entity/later-article.entity";
 import { ForumModule } from '../forum/forum.module';
 import { RecommendationArticle } from './entity/recommendation-article.entity';
+import { ThemeModule } from '../theme/theme.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ Article, LikeArticle, ThemeArticle, ArticleImg, LaterArticle, RecommendationArticle ]), ForumModule],
+  imports: [TypeOrmModule.forFeature([ Article, LikeArticle, ThemeArticle, ArticleImg, LaterArticle, RecommendationArticle ]), ForumModule, ThemeModule],
   controllers: [ArticleController],
   providers: [ArticleService]
 })
